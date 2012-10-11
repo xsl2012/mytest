33f35a0ff7f6a54366e343a141177e540121b13b
xsl <green_apple_2009@126.com>
2012-10-11 10:26:38 +0800

    添加了一个index.php文件

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2 style="color: red">hello,world</h2>
+        <?php
+            
+        ?>
+    </body>
+</html>
